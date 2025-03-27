6269a90b21bd12129edf0791d5dc32f17ef5007a
miyeon <1234heny@naver.com>
2025-03-27 15:07:10 +0900

    [feat] 2주차 Chapter2 안드로이드 프로젝트 업로드

---
+++ 면_이미연/2주차/Chapter2/app/src/main/java/com/example/chapter2/LocationFragment.kt
@@ -0,0 +1,17 @@
+package com.example.chapter2.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.chapter2.R
+
+class LocationFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_location, container, false)
+    }
+}
